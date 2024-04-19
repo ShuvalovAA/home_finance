41b2558cfe055b3bb40da929e19e83a55c5f6d79
Artemiy_Shuvalov <a.shuvalov@sailplay.ru>
2024-04-19 10:43:21 +0300

    убираешь лишнее по дашбордам

--- hfin/static/report/js/common.js
+++ hfin/static/report/js/common.js
@@ -190,56 +190,56 @@ function set_data_for_dashboard(start = null, end = null) {
   }
 /*Дашборд */
 
-function set_year_list(years){
-  //установить список годов в кнопку
-  div_options = document.getElementById('year_for_report')
-  for(i=0; i < years.length; i++){
-    year = years[i]
-    opt = document.createElement('option')
-    opt.textContent = year
-    opt.value = year
-    div_options.append(opt)
-  }  
-}
+// function set_year_list(years){
+//   //установить список годов в кнопку
+//   div_options = document.getElementById('year_for_report')
+//   for(i=0; i < years.length; i++){
+//     year = years[i]
+//     opt = document.createElement('option')
+//     opt.textContent = year
+//     opt.value = year
+//     div_options.append(opt)
+//   }  
+// }
 
-function get_years_list(){
-//получилить список годов, которые есть в доходах и расходах
-  $.ajax({
-    url: '/reporter/get_years_list',
-    method: 'GET',
-    dataType: 'json',
-    headers: {
-        'X-CSRFToken': get_token()
-    },
-    data: {'user_id': get_user_id()},
-    success: function(years) {
-      set_year_list(years)
-    }
-  });  
-}
+// function get_years_list(){
+// //получилить список годов, которые есть в доходах и расходах
+//   $.ajax({
+//     url: '/reporter/get_years_list',
+//     method: 'GET',
+//     dataType: 'json',
+//     headers: {
+//         'X-CSRFToken': get_token()
+//     },
+//     data: {'user_id': get_user_id()},
+//     success: function(years) {
+//       set_year_list(years)
+//     }
+//   });  
+// }
 
-function get_year_from_button(){
-  //забрать год из кнопки
-  button = document.getElementById('year_for_report')
-  value = button.selectedOptions[0].value
-  return value
-}
-function prepare_for_years_report(){
-//подготовить всё для работы с отчётом за год
-  year = get_year_from_button()
-  if(!year){
-    return
-  }
-  get_dataset(year)
-}
-function set_event_by_get_inc_exp_data(){
-  //повесить событие клика на кнопку формирирования датасета
-  button = document.getElementById('prepare_dataset')
-  button.addEventListener('click', function(e){
-    prepare_for_years_report()
-  })
+// function get_year_from_button(){
+//   //забрать год из кнопки
+//   button = document.getElementById('year_for_report')
+//   value = button.selectedOptions[0].value
+//   return value
+// }
+// function prepare_for_years_report(){
+// //подготовить всё для работы с отчётом за год
+//   year = get_year_from_button()
+//   if(!year){
+//     return
+//   }
+//   get_dataset(year)
+// }
+// function set_event_by_get_inc_exp_data(){
+//   //повесить событие клика на кнопку формирирования датасета
+//   button = document.getElementById('prepare_dataset')
+//   button.addEventListener('click', function(e){
+//     prepare_for_years_report()
+//   })
 
-}
+// }
 function get_dataset(){
   //получить набор данных по расходам и доходам за соответствующий год
   $.ajax({
